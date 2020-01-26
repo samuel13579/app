80cb844d9b1337986beb7dc444b5e13665fa6a26
Zamo141 <dzamo141@gmail.com>
2020-01-25 19:24:49 -0800

    Added login api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://TestAdmin:Bestpassword135@perryprocesses-tmtzr.mongodb.net/testretryWrites=true&w=majority';
+const client = new MongoClient(url);
+client.connect();
+
 const app = express();
+
 app.use(bodyParser.json());
 app.use((req, res, next) => 
 {
@@ -13,7 +20,30 @@ app.use((req, res, next) =>
     'Access-Control-Allow-Methods',
     'GET, POST, PATCH, DELETE, OPTIONS'
   );
-  res.status(200).send('API Workin');
+  app.post('/api/login', async(req, res, next) => 
+  {
+    const {login, password} = req.body;
+    const db = client.db();
+    // May need to change 'Users', Login:login and Password:password 
+    // depending on the collection w/in MongoDB
+    const results = await db.collection('Users').find({Login:login, Password:password}).toArray();
+    
+    var id = -1;
+    var firstName = '';
+    var lastName = '';
+
+    
+    if(results.length > 0){
+      id = results[0].UserId;
+      fName = results[0].FirstName;
+      lName = results[0].LastName;
+    }
+
+    //Returns a json with the user information back to the frontend
+    var ret = {id:id, firstName:fName, lastName:lName, error:''};
+    res.status(200).json(ret);
+  });
+  res.status(200).send('API Workin\n');
   next();
 });
 app.listen(5000); // start Node + Express server on port 5000

52174901db0d65231a523c0edb734e0ac21b67a7
masonproco <masonproco@knights.ucf.edu>
2020-01-28 10:04:15 -0500

    Set up login button to transfer to next page. Needs next page to set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import LoginCredentials from "./components/LoginCredentials";
+import PassWordBox from "./components/PassWordBox";
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LoginCredentials}/>  
+          {/* <Route exact path="/yikes" component={PassWordBox}/>  Hook up next page */}
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/LoginCredentials.js
+++ frontend/src/components/LoginCredentials.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react';
+import {Route, Router, Link, BrowserRouter} from 'react-router-dom';
 import { Form, Container, FormGroup, Label, Input, Button, ButtonGroup, Jumbotron, Fade } from 'reactstrap';
 import { render } from '@testing-library/react';
 import 'bootstrap/dist/css/bootstrap.css';
+import PassWordBox from './PassWordBox';
 
 class LogInCredentials extends Component {
 
+    login()
+    {
+
+    }
+
     render() {
         return(
-            <Container>
-                <div>
-                <Jumbotron fluid>
-                    <Container fluid>
-                        <h1 className="display-3">Contact Manager</h1>
-                    </Container>
-                </Jumbotron>
-                </div>
+            <div>
+                <Container>
+                    <div>
+                    <Jumbotron fluid>
+                        <Container fluid>
+                            <h1 className="display-3">Contact Manager</h1>
+                        </Container>
+                    </Jumbotron>
+                    </div>
 
-                <Form>
-                    <br></br>
-                    <FormGroup>
-                        <Label for="username" className="float-left" size = "lg">Username:</Label>
-                        <Input type="text" name="username" id="username" placeholder="Please enter your username"></Input>
-                    </FormGroup>
-                    <br></br>
-                    <FormGroup>
-                        <Label for="password" className="float-left" size = "lg">Password:</Label>
-                        <Input type="password" name="password" id="password" placeholder="Please enter your password"></Input>
-                    </FormGroup>
-                </Form>
-
-                <ButtonGroup>
-                    <Button color="primary" size="lg">Log In</Button>{''}
-                </ButtonGroup>
-            </Container>
+                    <Form>
+                        <br></br>
+                        <FormGroup>
+                            <Label for="username" className="float-left" size = "lg">Username:</Label>
+                            <Input type="text" name="username" id="username" placeholder="Please enter your username"></Input>
+                        </FormGroup>
+                        <br></br>
+                        <FormGroup>
+                            <Label for="password" className="float-left" size = "lg">Password:</Label>
+                            <Input type="password" name="password" id="password" placeholder="Please enter your password"></Input>
+                        </FormGroup>
+                    </Form>
+                        <ButtonGroup>
+                            {/* <Link to ="/yikes"><Button color="primary" size="lg">Log In</Button>{''}</Link> Hook up next page*/}
+                            <Button color="primary" size="lg">Log In</Button>{''}
+                        </ButtonGroup>
+                </Container>
+            </div>
         );
     }
 }

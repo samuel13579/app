7b06fdcf11431f35d89d044e480b9230e8f8b96a
Zamo141 <dzamo141@gmail.com>
2020-02-03 18:27:34 -0800

    Added LoginAPI, payload signing, and a "/me" endpoint. Can change upon  request

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 const User = require("../model/User");
 
@@ -42,6 +44,19 @@ router.post(
                     id: user.id
                 }
             };
+
+            jwt.sign(
+                payload,
+                "ILikeTurtles", {
+                    expiresIn: 10000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
         } catch (err) {
             console.log(err.message);
             res.status(500).send("Error in Saving");
@@ -49,4 +64,76 @@ router.post(
     }
 );
 
+router.post(
+    "/login",
+    async (req, res) => {
+        const {
+            username, 
+            password
+        } = req.body;
+        
+        try {
+            let user = await User.findOne({
+                username
+            });
+            
+            if(!user)
+                return res.status(400).json({
+                    message: "Username not found"
+                });
+            
+            //This rehashes the password to compare with the hashed password in the db
+            const checkPassword = await bcrypt.compare(password, user.password);
+            
+            if(!checkPassword)
+                return res.status(400).json({
+                    message: "Incorrect Password"
+                });
+            
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+            
+            //This signs the payload 
+            jwt.sign(
+                payload,
+                "ILikeTurtles",
+                {
+                    expiresIn: 3600
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({
+                message: "Server Error"
+            });
+        }
+    }
+);
+
+/**
+ * @method - GET
+ * @description - Get LoggedIn User
+ * @param - /me
+ */
+
+
+router.get("/me", auth, async (req, res) => {
+    try {
+      // request.user is getting fetched from Middleware after token authentication
+      const user = await User.findById(req.user.id);
+      res.json(user);
+    } catch (e) {
+      res.send({ message: "Error in Fetching user" });
+    }
+  });
+  
 module.exports = router;
